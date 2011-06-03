47319b058889dc6f1aaf4f95fbb2d79b513c504e
Wim Meeussen <wim@willowgarage.com>
2011-06-03 09:32:52 -0700

    add deprecation warnings to old includes

--- urdf/include/urdf/color.h
+++ urdf/include/urdf/color.h
@@ -1,1 +1,3 @@
+#pragma message("WARNING: Including urdf/color.h is deprecated. Include urdf_parser/color.h instead")
+
 #include "urdf_parser/color.h"
--- urdf/include/urdf/joint.h
+++ urdf/include/urdf/joint.h
@@ -1,1 +1,4 @@
+#pragma message("WARNING: Including urdf/joint.h is deprecated. Include urdf_parser/joint.h instead")
+
+
 #include "urdf_parser/joint.h"
