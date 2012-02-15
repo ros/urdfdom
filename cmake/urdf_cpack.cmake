3af98a023150e31ec2b2e8edfaef911923fb6d24
John Hsu <johnhsu@willowgarage.com>
2012-02-14 19:17:17 -0800

    update makefiles

--- cmake/urdf_cpack.cmake
+++ cmake/urdf_cpack.cmake
@@ -18,29 +18,29 @@ include (InstallRequiredSystemLibraries)
 #execute_process(COMMAND dpkg --print-architecture _NPROCE)
 set (DEBIAN_PACKAGE_DEPENDS "libogre-dev, libfreeimage-dev, libqt4-dev, libprotobuf-dev, libprotoc-dev, libtbb2, libxml2, libboost-all-dev")
 
-set (GAZEBO_CPACK_CFG_FILE "${PROJECT_BINARY_DIR}/cpack_options.cmake")
+set (URDF_CPACK_CFG_FILE "${PROJECT_BINARY_DIR}/cpack_options.cmake")
 
 ################################################################################
 # Make the CPack input file
-#macro(GAZEBO_MAKE_CPACK_INPUT)
-#  set(_cpack_cfg_in "${gazebo_cmake_dir}/cpack_options.cmake.in")
+#macro(URDF_MAKE_CPACK_INPUT)
+#  set(_cpack_cfg_in "${urdf_cmake_dir}/cpack_options.cmake.in")
 #
 #  #Prepare the components list
-#  #GAZEBO_CPACK_MAKE_COMPS_OPTS(GAZEBO_CPACK_COMPONENTS "${_comps}")
+#  #URDF_CPACK_MAKE_COMPS_OPTS(URDF_CPACK_COMPONENTS "${_comps}")
 #
-#  configure_file(${_cpack_cfg_in} ${GAZEBO_CPACK_CFG_FILE} @ONLY)
-#endmacro(GAZEBO_MAKE_CPACK_INPUT)
+#  configure_file(${_cpack_cfg_in} ${URDF_CPACK_CFG_FILE} @ONLY)
+#endmacro(URDF_MAKE_CPACK_INPUT)
 
 
-#macro(GAZEBO_CPACK_MAKE_COMPS_OPTS _var _current)
+#macro(URDF_CPACK_MAKE_COMPS_OPTS _var _current)
 #    set(_comps_list)
-#    foreach(_ss ${GAZEBO_SUBSYSTEMS})
-#        GAZEBO_GET_SUBSYS_STATUS(_status ${_ss})
+#    foreach(_ss ${URDF_SUBSYSTEMS})
+#        URDF_GET_SUBSYS_STATUS(_status ${_ss})
 #        if(_status)
 #            set(_comps_list "${_comps_list} ${_ss}")
-#            GAZEBO_CPACK_ADD_COMP_INFO(${_var} ${_ss})
+#            URDF_CPACK_ADD_COMP_INFO(${_var} ${_ss})
 #        endif(_status)
 #    endforeach(_ss)
 #    set(${_var} "${${_var}}\nset(CPACK_COMPONENTS_ALL${_comps_list})\n")
-#endmacro(GAZEBO_CPACK_MAKE_COMPS_OPTS)
+#endmacro(URDF_CPACK_MAKE_COMPS_OPTS)
 
