5c453c54f1c77c64c06774d398c1d53c405f9ef6
Ioan Sucan <isucan@willowgarage.com>
2012-05-22 19:38:14 -0700

    simplify build system

--- cmake/SearchForTinyXML.cmake
+++ cmake/SearchForTinyXML.cmake
@@ -1,4 +1,3 @@
-include (${urdf_cmake_dir}/URDFUtils.cmake)
 include (CheckCXXSourceCompiles)
 
 set (tinyxml_include_dirs "" CACHE STRING "Tinyxml include paths. Use this to override automatic detection.")
@@ -6,13 +5,9 @@ set (tinyxml_library_dirs "" CACHE STRING "Tinyxml library paths. Use this to ov
 set (tinyxml_libraries "" CACHE STRING "Tinyxml libraries Use this to override automatic detection.")
 set (tinyxml_cflags "" CACHE STRING "Tinyxml Use this cflag to enable string support.")
 
-set (boost_include_dirs "" CACHE STRING "Boost include paths. Use this to override automatic detection.")
-set (boost_library_dirs "" CACHE STRING "Boost library paths. Use this to override automatic detection.")
-set (boost_libraries "" CACHE STRING "Boost libraries. Use this to override automatic detection.")
-
 SET (urdf_lflags "" CACHE STRING "Linker flags such as rpath for URDF executable.")
 
-include (${urdf_cmake_dir}/FindOS.cmake)
+include (FindOS)
 include (FindPkgConfig)
 
 ########################################
@@ -70,50 +65,3 @@ else (PKG_CONFIG_FOUND)
   set (BUILD_URDF OFF CACHE INTERNAL "Build URDF" FORCE)
   BUILD_ERROR ("Error: pkg-config not found")
 endif (PKG_CONFIG_FOUND)
-
-find_package(GTest)
-if (GTEST_FOUND)
-  enable_testing()
-else()
-  message (STATUS "  Tests will not be built")
-endif()
-
-########################################
-# Find Boost, if not specified manually
-if (NOT boost_include_dirs AND NOT boost_library_dirs AND NOT boost_libraries )
-
-  # Clear some variables to ensure that the checks for boost are 
-  # always run
-  set (Boost_THREAD_FOUND OFF CACHE INTERNAL "" FORCE)
-  set (Boost_SIGNALS_FOUND OFF CACHE INTERNAL "" FORCE)
-
-  set(Boost_ADDITIONAL_VERSIONS "1.35" "1.35.0" "1.36" "1.36.1" "1.37.0" "1.39.0" CACHE INTERNAL "Boost Additional versions" FORCE)
-
-  include (FindBoost)
-  find_package(Boost ${MIN_BOOST_VERSION} REQUIRED thread signals system filesystem)
-
-  if (NOT Boost_FOUND)
-    set (BUILD_URDF OFF CACHE INTERNAL "Build URDF" FORCE)
-    BUILD_ERROR ("Boost thread and signals not found. Please install Boost threads and signals version ${MIN_BOOST_VERSION} or higher.")
-  endif (NOT Boost_FOUND)
-
-  set (boost_include_dirs ${Boost_INCLUDE_DIRS} CACHE STRING 
-    "Boost include paths. Use this to override automatic detection." FORCE)
-
-  set (boost_library_dirs ${Boost_LIBRARY_DIRS} CACHE STRING
-    "Boost link dirs. Use this to override automatic detection." FORCE)
-
-  LIST_TO_STRING(tmp "${Boost_LIBRARIES}")
-  set (boost_libraries ${tmp} CACHE STRING 
-    "Boost libraries. Use this to override automatic detection." FORCE )
-
-endif (NOT boost_include_dirs AND NOT boost_library_dirs AND NOT boost_libraries ) 
-
-set (Boost_DIR "" CACHE INTERNAL "" FORCE)
-
-STRING(REGEX REPLACE "(^| )-L" " " boost_library_dirs "${boost_library_dirs}")
-STRING(REGEX REPLACE "(^| )-l" " " boost_libraries "${boost_libraries}")
-#STRING(STRIP ${boost_library_dirs} boost_library_dirs)
-#STRING(STRIP ${boost_libraries} boost_libraries)
-STRING(REGEX REPLACE " " ";" boost_libraries "${boost_libraries}")
-
