53a8ee268f6a369f80d711e6ce8b2d70ab28fbad
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2015-11-07 00:41:51 +0100

    proper unittest for urdf_parser_py
    - added transmission parsing tests

---
+++ urdf_parser_py/test/test_urdf.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+
+import unittest
+from xml.dom import minidom
+from xml_matching import xml_matches
+from urdf_parser_py import urdf
+
+class TestURDFParser(unittest.TestCase):
+    def parse_and_compare(self, orig):
+        xml = minidom.parseString(orig)
+        robot = urdf.Robot.from_xml_string(orig)
+        rewritten = minidom.parseString(robot.to_xml_string())
+        self.assertTrue(xml_matches(xml, rewritten))
+
+    def test_new_transmission(self):
+        xml = '''<?xml version="1.0"?>
+<robot name="test">
+  <transmission name="simple_trans">
+    <type>transmission_interface/SimpleTransmission</type>
+    <joint name="foo_joint">
+      <hardwareInterface>EffortJointInterface</hardwareInterface>
+    </joint>
+    <actuator name="foo_motor">
+      <mechanicalReduction>50.0</mechanicalReduction>
+    </actuator>
+  </transmission>
+</robot>'''
+        self.parse_and_compare(xml)
+
+    def test_old_transmission(self):
+        xml = '''<?xml version="1.0"?>
+<robot name="test">
+  <transmission name="PR2_trans" type="SimpleTransmission">
+    <joint name="foo_joint"/>
+    <actuator name="foo_motor"/>
+    <mechanicalReduction>1.0</mechanicalReduction>
+  </transmission>
+</robot>'''
+        self.parse_and_compare(xml)
+
+
+if __name__ == '__main__':
+    unittest.main()
