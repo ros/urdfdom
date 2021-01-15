99c6f58120a5dfd1f9fc36419cd9f9aa0246efde
Jacob Perron <jacob@openrobotics.org>
2021-01-15 09:12:00 -0800

    Put the 'v' back in graphviz (#152)
    * Renamed the .cpp file.
    * Add executable correctly spelling graphviz
    * Add deprecation warning if the old executable name is used.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- urdf_parser/src/urdf_to_graphviz.cpp
+++ urdf_parser/src/urdf_to_graphviz.cpp
@@ -85,15 +85,21 @@ void printTree(LinkConstSharedPtr link, string file)
 
 int main(int argc, char** argv)
 {
+  std::string executable_name(argv[0]);
+  std::string deprecated_name("urdf_to_graphiz");
+  if (deprecated_name == executable_name.substr(executable_name.size() - deprecated_name.size())) {
+    std::cerr << "WARNING: The executable named '" << deprecated_name
+              << "' is deprecated. Use 'urdf_to_graphviz' instead." << std::endl;
+  }
   if (argc < 2 || argc > 3) {
-    std::cerr << "Usage: urdf_to_graphiz input.xml [OUTPUT]"
+    std::cerr << "Usage: urdf_to_graphviz input.xml [OUTPUT]"
               << "  Will create either $ROBOT_NAME.gv & $ROBOT_NAME.pdf in CWD"
               << "  or OUTPUT.gv & OUTPUT.pdf." << std::endl;
     return -1;
   }
   if (argc != 3) {
     std::cerr << "WARNING: OUTPUT not given. This type of usage is deprecated!"
-              << "Usage: urdf_to_graphiz input.xml [OUTPUT]"
+              << "Usage: urdf_to_graphviz input.xml [OUTPUT]"
               << "  Will create either $ROBOT_NAME.gv & $ROBOT_NAME.pdf in CWD"
               << "  or OUTPUT.gv & OUTPUT.pdf." << std::endl;
   }
